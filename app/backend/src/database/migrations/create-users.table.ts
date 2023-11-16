4d80d6fa26c2f39beadd3b6992c15c0049e64fee
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-11-16 21:00:45 +0000

    feat: requisito 6

---
+++ app/backend/src/database/migrations/create-users.table.ts
@@ -0,0 +1,35 @@
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import { IUser } from '../../Interfaces/IUser';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IUser>>('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('users');
+  }
+}
